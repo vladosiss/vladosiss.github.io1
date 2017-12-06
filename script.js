a7c7454eda145df4641f295b7d39941e12f45b09
vladosiss_ronin <vlad3a@i.ua>
2017-12-06 06:43:00 +0200

    Revert "menuTZ"
    This reverts commit 9dff4cfea058259245175fdd07e24ed0eebc4652.

--- script.js
+++
@@ -1,8 +0,0 @@
-function myFunction (){
-var x=document.getElementById("myTopnav");
-if( x.className === "mainMenu__topnav"){
-    x.className +=" responsive";
-}else{
-x.className ="mainMenu__topnav";
-}
-}
